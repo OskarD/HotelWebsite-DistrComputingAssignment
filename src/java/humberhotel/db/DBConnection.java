010e6b5e342715dba0fe5333d108250581431016
Oskar Danielsson <oskar@smsgrupp.se>
2015-04-11 18:04:15 -0400

    Replaced DBConnection class with mine from the first lab

--- src/java/humberhotel/db/DBConnection.java
+++ src/java/humberhotel/db/DBConnection.java
@@ -1,35 +1,47 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package humberhotel;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 /**
  *
- * @author Serio
+ * @author Oskar
  */
 public class DBConnection {
-    private static volatile Connection dbc;
+    private static volatile DBConnection instance;
     
-    public static Connection getConnection() {
-        if (dbc == null) {
-            synchronized (DBConnection.class) {
-                if (dbc == null) {
-                    try {
-                        Class.forName("oracle.jdbc.OracleDriver");
-                        dbc = DriverManager.getConnection("jdbc:oracle:thin:@dilbert.humber.ca:1521:grok", "N00770693", "oracle");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+    private static Connection connection;
+    
+    private DBConnection() { 
+        try {
+            // Load driver
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+
+            // Get a Connection to the database
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@dilbert.humber.ca:1521:grok", "N00770693", "oracle");
+        
+            System.out.println("Connection successful!");
+        } catch (Exception ex) {
+            System.out.println("Could not connect to the database.");
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return dbc;
     }
     
-    
+    public static Connection getConnection() {
+        if(instance == null) {
+            synchronized (DBConnection.class) {
+                if(instance == null)
+                    instance = new DBConnection();
+            }
+        }
+        
+        return connection;
+    }
 }
