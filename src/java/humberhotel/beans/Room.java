7eb4badde7674c756f0fa829cbb6260461f9e347
Oskar Danielsson <oskar@smsgrupp.se>
2015-04-12 00:41:34 -0400

    Adjusted constants' names Added create and delete methods to Room class

--- src/java/humberhotel/beans/Room.java
+++ src/java/humberhotel/beans/Room.java
@@ -5,11 +5,25 @@
  */
 package humberhotel.beans;
 
+import humberhotel.db.DBConnection;
+import humberhotel.exception.HotelException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Oskar
  */
 public class Room {
+    private static final String 
+        QUERY_CREATE = 
+            "INSERT INTO hotelrooms (roomnumber, type) VALUES (?, ?)",
+        QUERY_DELETE =
+            "DELETE FROM hotelrooms WHERE roomnumber = ?";
+    
     private int
             roomNumber;
     
@@ -34,5 +48,27 @@ public class Room {
 
     public void setType(String type) {
         this.type = type;
+    }
+    
+    public static final void create(int roomNumber, String type) throws SQLException {
+        try (PreparedStatement stmt = DBConnection.getConnection().prepareStatement(QUERY_CREATE)) {
+            stmt.setInt(1, roomNumber);
+            stmt.setString(2, type);
+            
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Booking.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        }
+    }
+    
+    public static final void delete(int roomNumber) throws SQLException {
+        try (PreparedStatement stmt = DBConnection.getConnection().prepareStatement(QUERY_DELETE)) {
+            stmt.setInt(1, roomNumber);
+            stmt.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(Booking.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        }
     }
 }
