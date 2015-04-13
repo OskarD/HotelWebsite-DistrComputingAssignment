a351b8f8503287c9d3f428035bf1a7207ae55bc3
Tyler Serio <tylerserio@hotmail.com>
2015-04-12 23:56:35 -0400

    Added Sorting algorithm  for sorting by room number.

--- src/java/humberhotel/servlets/ReportServlet.java
+++ src/java/humberhotel/servlets/ReportServlet.java
@@ -5,8 +5,13 @@
  */
 package humberhotel.servlets;
 
+import humberhotel.beans.Booking;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -37,10 +42,38 @@ public class ReportServlet extends HttpServlet {
             out.println("<h2>Room Reports</h2>");
             out.println("<h3>Sort By:</h3>");
             out.println("<p><a href='reports.jsp?report=newestdate'>Latest Bookings</a> - <a href='reports.jsp?report=numerical'>Numerical Order</a> - <a href='reports.jsp?report=price'>Price</a></p>");
-            if (request.getParameter("report").equalsIgnoreCase("newestdate")) {
-                out.println("<p>Newest Dates</p>");
-            } else if (request.getParameter("report") == null || request.getParameter("report").equalsIgnoreCase("")) {
+            if (request.getParameter("report") != null) {
+                if (request.getParameter("report").equalsIgnoreCase("newestdate")) {
+                    out.println("<p>Newest Dates</p>");
+                } else if (request.getParameter("report").equalsIgnoreCase("numerical")) {
+                    out.println("<p>Numerical Order</p>");
 
+                } else if (request.getParameter("report").equalsIgnoreCase("price")) {
+                    ArrayList<Booking> bookedRooms = new ArrayList<>();
+                    ArrayList<Booking> bookedRoomsSorted = new ArrayList<>();
+                    try {
+                        bookedRooms = Booking.getAll();
+                    } catch (SQLException ex) {
+                        Logger.getLogger(ReportServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    for (Booking b : bookedRooms) {
+                        if (bookedRoomsSorted.isEmpty()) {
+                            bookedRoomsSorted.add(b);
+                        } else {
+                            for (int i = 0; i < bookedRoomsSorted.size(); i++) {
+                                if (b.getRoomNumber() > bookedRoomsSorted.get(i).getRoomNumber()) {
+                                    Booking replaced = bookedRoomsSorted.get(i);
+                                    bookedRoomsSorted.remove(i);
+                                    bookedRoomsSorted.add(b);
+                                    bookedRoomsSorted.add(replaced);
+                                }
+                            }
+                        }
+                    }
+                    for (Booking b : bookedRooms) {
+                        out.println("<p>Room Number: <b>" + b.getRoomNumber() + "</b> (booked by <b>" + b.getBookedBy() + "</b>)</p>");
+                    }
+                }
             }
             out.println("</div>");
             request.getRequestDispatcher("/footer.jsp").include(request, response);                            
